1a78bf943b2be185aa0a58703186c19ff286f8a1
Shinya Sugamoto <sugamoto@me.com>
2021-03-02 11:22:38 +0000

    feat: add tun/tap device controller

---
+++ pkg/raw/tuntap/tuntap.go
@@ -0,0 +1,29 @@
+package tuntap
+
+import "io"
+
+type Tap struct {
+	io.ReadWriteCloser
+	name string
+}
+
+func NewTap(name string) (*Tap, error) {
+	n, f, err := openTap(name)
+	if err != nil {
+		return nil, err
+	}
+	return &Tap{
+		// TODO: Can file descripter be io.ReadWriteCloser?
+		ReadWriteCloser: f,
+		name:            n,
+	}, nil
+}
+
+func (t Tap) Address() []byte {
+	addr, _ := getAddress(t.name)
+	return addr[:6]
+}
+
+func (t Tap) Name() string {
+	return t.name
+}
---
+++ pkg/raw/tuntap/tuntap_linux.go
@@ -0,0 +1,44 @@
+package tuntap
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/gasugesu/lectcp/pkg/ioctl"
+)
+
+const cloneDevice = "/dev/net/tun"
+
+func openTap(name string) (string, *os.File, error) {
+	if len(name) >= syscall.IFNAMSIZ {
+		return "", nil, fmt.Errorf("name is too long")
+	}
+	file, err := os.OpenFile(cloneDevice, os.O_RDWR, 0600)
+	if err != nil {
+		return "", nil, err
+	}
+	name, err = ioctl.TUNSETIFF(file.Fd(), name, syscall.IFF_TAP|syscall.IFF_NO_PI)
+	if err != nil {
+		return "", nil, err
+	}
+	flags, err := ioctl.SIOCGIFFLAGS(name)
+	if err != nil {
+		file.Close()
+		return "", nil, err
+	}
+	flags |= (syscall.IFF_UP | syscall.IFF_RUNNING)
+	if err := ioctl.SIOCSIFFLAGS(name, flags); err != nil {
+		file.Close()
+		return "", nil, err
+	}
+	return name, file, nil
+}
+
+func getAddress(name string) ([]byte, error) {
+	addr, err := ioctl.SIOCGIFHWAADDR(name)
+	if err != nil {
+		return nil, err
+	}
+	return addr, nil
+}
