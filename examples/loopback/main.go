d2a8f7ec03f3b22b2e4a74faea676b888b8da967
Shinya Sugamoto <sugamoto@me.com>
2021-03-03 18:28:56 +0000

    feat: add loopback examples

---
+++ examples/loopback/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/gasugesu/lectcp/pkg/arp"
+	"github.com/gasugesu/lectcp/pkg/icmp"
+	"github.com/gasugesu/lectcp/pkg/ip"
+	"github.com/gasugesu/lectcp/pkg/loopback"
+	"github.com/gasugesu/lectcp/pkg/net"
+)
+
+var sig chan os.Signal
+
+func init() {
+	arp.Init()
+	icmp.Init()
+}
+
+func setup() (*net.Device, error) {
+	// sig handling
+	sig = make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	link, err := loopback.NewDevice()
+	if err != nil {
+		return nil, err
+	}
+	dev, err := net.RegisterDevice(link)
+	if err != nil {
+		return nil, err
+	}
+	iface, err := ip.CreateInterface(dev, "127.0.0.1", "255.0.0.0", "")
+	if err != nil {
+		return nil, err
+	}
+	dev.RegisterInterface(iface)
+	return dev, nil
+}
+
+func main() {
+	dev, err := setup()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("[%s] %s\n", dev.Name(), dev.Address())
+	for _, iface := range dev.Interfaces() {
+		fmt.Printf("  - %s\n", iface.Address())
+	}
+	// launch send loop
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		t := time.NewTicker(1 * time.Second)
+		defer t.Stop()
+		peer := ip.ParseAddress("127.0.0.1")
+		data := []byte("1234567890")
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-t.C:
+				fmt.Printf("send ICMP Echo to %s\n", peer)
+				icmp.EchoRequest(data, peer)
+			}
+		}
+	}()
+	select {
+	case s := <- sig:
+		fmt.Printf("sig: %s\n", s)
+		cancel()
+	}
+	wg.Wait()
+	dev.Shutdown()
+	fmt.Println("good bye")
+}
